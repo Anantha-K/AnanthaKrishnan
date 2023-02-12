5958757d37a71be6b4ee1bfc37558035884332ca
Anantha-K <89961607+Anantha-K@users.noreply.github.com>
2023-02-12 18:39:39 +0530

    Add files via upload

--- pages/Work.js
+++ pages/Work.js
@@ -32,7 +32,7 @@ const Work = () => {
                   width={300}
                   height={300}
                   className="object-cover w-full h-56 "
-                  src="https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src="https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?w=2000&t=st=1676206482~exp=1676207082~hmac=e820b48488ff2121df4ef031f37d5f5345d7f18ea856422b6f842e0558f690e8"
                   alt="avatar"
                 ></Image>
 
@@ -67,10 +67,10 @@ const Work = () => {
             >
               <div className="w-full  mb-4  hover:shadow-2xl cursor-pointer max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 ml-3 mr-4">
                 <Image
-                  width={100}
-                  height={100}
+                  width={400}
+                  height={400}
                   className="object-cover w-full h-56"
-                  src="https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                  src="https://img.freepik.com/free-vector/app-development-illustration_52683-47931.jpg?w=2000&t=st=1676207197~exp=1676207797~hmac=54bdd8e9280cf2c65cffffef9914eddcc3ef084b7081cce36bcee43cd4bd4797"
                   alt="avatar"
                 ></Image>
 
@@ -103,13 +103,13 @@ const Work = () => {
               }}
             >
               <div className="w-full max-w-xs mb-4  hover:shadow-2xl cursor-pointer overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 ml-3 mr-4">
-                {/* <Image
+                <Image
                   width={100}
                   height={100}
                   className="object-cover w-full h-56"
-                  src="https://img.freepik.com/free-vector/marketing-concept-illustration_114360-3903.jpg?size=626&ext=jpg&ga=GA1.2.267023450.1675486331&semt=ais"
+                  src="https://img.freepik.com/free-vector/marketing-consulting-concept-illustration_114360-9027.jpg?w=2000&t=st=1676207299~exp=1676207899~hmac=1c1b9e9f2cea1ffe69df8ffc66340aea3e1a9a9badf1575871009a7fca1eb86a"
                   alt="avatar"
-                ></Image> */}
+                ></Image>
 
                 <div className="py-5 text-center">
                   <a
